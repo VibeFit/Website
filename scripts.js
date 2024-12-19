1d235d5282ad7fb5703b48c02edcfbf4bbf2744c
VibeFit <vibefit.couch.company@gmail.com>
2024-12-19 21:58:15 +0530

    Initial Landing Page

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.body.classList.add("loaded"); // Add class to trigger transitions
+    }, 2000); // 1 second delay for animation
+});
