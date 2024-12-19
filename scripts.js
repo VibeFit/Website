0c69162852cad4969071a71f31f80ebd0abbaa92
VibeFit <vibefit.couch.company@gmail.com>
2024-12-20 00:11:54 +0530

    Casaurol added

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.add("loaded"); // Add class to trigger transitions
     }, 2000); // 1 second delay for animation
 });
+
+
+let currentIndex = 0;
+
+const images = document.querySelectorAll('.carousel img');
+const totalImages = images.length;
+
+function changeImage() {
+  currentIndex = (currentIndex + 1) % totalImages;
+  const offset = -currentIndex * 100;
+  document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+// Change image every 3 seconds
+setInterval(changeImage, 3000);
